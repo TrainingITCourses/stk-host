0461bf43bcc647d0714b7193361b5e2e582c3550
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-18 17:58:12 +0200

    feat: host

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { PlatformStore } from '@stk/srv';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, JsonPipe],
   template: `
-    <h1>Welcome to {{ title }}!</h1>
-
+    <h1 routerLink="/">Welcome to {{ title }}!</h1>
+    <nav>
+      <p><a routerLink="remote/alfa">Remote alfa</a></p>
+      <p>
+        <a routerLink="remote/beta" [queryParams]="{ q: 'Curso de softtek' }"
+          >Remote beta</a
+        >
+      </p>
+    </nav>
     <router-outlet />
+    <hr />
+    <p>Platform data:</p>
+    <pre>{{ data() | json }}</pre>
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'lab-host';
+  #platform = inject(PlatformStore);
+  data = this.#platform.data;
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+const mfe2 = 'mfe2';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
       import('./routes/home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'remote',
+    loadChildren: () =>
+      loadRemoteModule(mfe2, './REMOTE_ROUTES').then((m) => m.REMOTE_ROUTES),
   },
 ];
